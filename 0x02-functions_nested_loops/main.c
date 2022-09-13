730ec5b6fef4a1e9f4aa942a7bc1e59ee4c64aa8
cryptorich1 <jevico1@gmail.com>
2022-09-13 15:06:34 -0700

    Main

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,24 @@
+#ifndef alx_h
+#define axl_h
+
+/**
+ * File: alx.h
+ * auth: Victor Ogbra
+ * Desc: Header file containing declarations for all functions
+ * 	used in the 2-functions_nested_loops
+ */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _isalpha(int c);
+int _islower(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(init);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+endif
