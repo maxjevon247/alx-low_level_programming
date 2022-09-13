d731644743b3d262980c3cad0f929346b3d6e7b1
cryptorich1 <jevico1@gmail.com>
2022-09-13 02:48:20 -0700

    prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "alx.h"
+/**
+ *
+ * main - prints the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; ;etter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
