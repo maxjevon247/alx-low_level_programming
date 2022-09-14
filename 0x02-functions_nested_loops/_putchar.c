5f369972668fd1ccd6c8083690f8b7be1735569b
cryptorich1 <jevico1@gmail.com>
2022-09-14 00:23:23 -0700

    putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "alx.h"
+#include <unistd.h>
+
+/**
+ * _putchar.c - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ * 	  On error, -1
+ */
+ int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
