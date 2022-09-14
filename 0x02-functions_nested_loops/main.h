7b2707a6508b6dd4b5f477a9b2a3550337f2155d
cryptorich1 <jevico1@gmail.com>
2022-09-14 01:05:58 -0700

    main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,24 +1,24 @@
-#ifndef main_h
-#define main_h
+#ifndef main.h
+#define main.h
 
 /**
+ *
  * File: main.h
- * auth: Victor Ogbra
  * Desc: Header file containing declarations for all functions
- * 	used in the 2-functions_nested_loops
+ *       used in the 0x02-functions_nested_loops directory.
  */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _isalpha(int c);
 int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(init);
+int print_last_digit(int);
 void jack_bauer(void);
-void time_table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-endif
+#endif
