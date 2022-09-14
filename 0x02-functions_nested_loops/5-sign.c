faf8413548ae57640c505a3ed052b2d32fb1ad34
cryptorich1 <jevico1@gmail.com>
2022-09-14 02:38:08 -0700

    nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,30 @@
 #include"main.h"
 
 /**
- * print_sign - prints the sign of a number
+ * print_sign - print + if n is greater than zero,
+ *             0 if n is zero and - if n is less
+ *             than zero.
  *
- * Return 1 if n > z
- *  	 0 if n =0
- *  	-1 if n < 0
+ * @n: takes integer type input for function.
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43 + 0);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(0 + 48);
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
+		_putchar(45 + 0);
 		return (-1);
+	}
 }
