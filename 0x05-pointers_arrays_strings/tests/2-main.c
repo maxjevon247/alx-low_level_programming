baa34ac00b14c44e9bc7efa0face01dae2e10d48
cryptorich1 <jevico1@gmail.com>
2022-09-19 18:00:53 -0700

    pointers, Arrays and strings

--- 0x05-pointers_arrays_strings/tests/2-main.c
+++ 0x05-pointers_arrays_strings/tests/2-main.c
@@ -1,17 +1,18 @@
 #include "../main.h"
 #include <stdio.h>
 
+int_strlen(char *);
 /**
- *  * main - check for main file.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check for Helberton School studednts.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char *str;
 	int len;
 
-	str = "Alx";
+	str = "Holberton!";
 	len =_strlen(str);
 	printf("%d\n", len);
 	return (0);
