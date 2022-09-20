2cc1b3510f96f74f2e51c582126c72416be46ce5
cryptorich1 <jevico1@gmail.com>
2022-09-20 15:22:21 -0700

    pointers, Arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,11 +3,9 @@
 /**
  * File: 7-puts_half.c
  * Auth: Ogbra Victor
- *
  * Desc: puts_half - Prints half of a string.
  * @str: The string to be printed
  */
-
 void puts_half(char *str)
 {
 	int index = 0, len = 0, n;
@@ -16,12 +14,12 @@ void puts_half(char *str)
 		len++;
 	
 	if((len % 2) == 0)
-		n = len / 2;
-	
+		n = len / 2;	
 	else
 		n = (len + 1) / 2;
 	
 	for (index = n; index < len; index++)
 		_putchar(str[index]);
+
 	_putchar('\n');
 }
