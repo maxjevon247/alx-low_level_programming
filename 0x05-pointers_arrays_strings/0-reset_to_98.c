4871ce504ad41309aefd9079b2a16f5b5a39286b
cryptorich1 <jevico1@gmail.com>
2022-09-19 17:44:55 -0700

    Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,14 @@
-/**File: 0-reset_to_98.c
- * Auth: Oggbra Victor
- */
-
 #include "main.h"
 
 /**
+ * File: 0-reset_to_98.c
+ * Auth: ggbra Victor
+ *
  * reset_to_98 - take a pointer to an int and updates
- * 		the value it point to 98.
+ *		the value it point to 98.
  * @n: The pointer to an int.
  */
 void reset_to_98(int *n)
 {
-	*n =98;
+	*n = 98;
 }
