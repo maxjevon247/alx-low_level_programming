99dcf7288f46dc57e5512374908e4cf0046c5071
cryptorich1 <jevico1@gmail.com>
2022-09-20 16:27:17 -0700

    pointers, Arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,10 @@
-/**
- *  * File: 9-strcpy.c
- *   * Auth: Ogbra Victor
- *    */
-
 #include "main.h"
 
 /**
- * _strcpy - Copies a string pointed to by @src, including the
+ * File: 9-strcpy.c
+ * Auth: Ogbra Victor
+ *
+ * Desc:  _strcpy - Copies a string pointed to by @src, including the
  *           terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
