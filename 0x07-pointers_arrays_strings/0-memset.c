f71b04bae693fe917b228f83dc37f90f72fe566e
cryptorich1 <jevico1@gmail.com>
2022-09-27 10:46:46 -0700

    first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int size = n;
-
-	if (size > 0)
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
 	{
-		int i;
-		for (i = 0; i < size; i++)
-			dest[i] = src[i];
+		dest[i] = src[i];
 	}
+
 	return (dest);
 }
