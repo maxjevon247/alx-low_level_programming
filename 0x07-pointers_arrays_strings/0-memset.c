319f5d49ce59d8a3372455f41545d338c9304ac4
cryptorich1 <jevico1@gmail.com>
2022-09-27 07:33:12 -0700

    first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,15 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	
-	for (i = 0; i < n; i++)
+	int size = n; /* only accept positive size */
+
+	if (size > 0)
 	{
+		int i;
+
+	for (i = 0; i < size; i++)
 		s[i] = b;
 	}
+
 	return (s);
 }
