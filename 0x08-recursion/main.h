4bd5f7ef0b371442bd5193035eef520e27a1ab29
cryptorich1 <jevico1@gmail.com>
2022-09-29 02:24:20 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*header file containing prototypes for all functions used in this project
+ */
+#include <stdlib.h>
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -10,3 +13,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int wildcmp(char *s1, char *s2);
 int is_palindrome(char *s);
+
+#endif
