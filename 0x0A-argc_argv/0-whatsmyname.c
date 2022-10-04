a37ca3ff6ec613c17665e3d6321ef713f52894a1
cryptorich1 <jevico1@gmail.com>
2022-10-04 02:51:51 -0700

    Program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * whatsmyname -program that prints its name, followed by a new line
+ *
+ * Return: Always return 0
+ */
+int main(int argc, char *argv[])
+{
+	putchar("Program's name is: %s\n",arg[])
+		return 0
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* main -Prototype file use in by other files in the program */
+#include <stdlib.h>
+
+#endif
