f966c7330153df5d4bfbc1765f014a017385c33c
cryptorich1 <jevico1@gmail.com>
2022-10-20 19:56:12 -0700

    singly linked project

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - print character to stdout
+ * @c: character to print
+ *
+ * Return: On success 1, otherwise -1 and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c 1));
+}
+
