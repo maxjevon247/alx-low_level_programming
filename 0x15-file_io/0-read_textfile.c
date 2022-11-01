6f70c4534eee57110f6fe31e8f15be15db170987
cryptorich1 <jevico1@gmail.com>
2022-11-01 03:08:20 -0700

    File read and write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reading a text file and prints it to the POSIX standard out
+ * @filename: file's name to read
+ * @letters: print the number of characters
+ *
+ * Return: 0 on success
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int txt_file, total, read_status;
+	char buffer[BUFSIZE];
+
+	if (filename == NULL)
+		return (0);
+	txt_file = open(filename, O_RDONLY);
+	if (txt_file == -1)
+		return (0);
+	total = 0;
+	read_status = 1;
+	while (letters > BUFSIZE && read_status != 0)
+	{
+		read_status = read(txt_file, buffer, BUFSIZE);
+		write(STDOUT_FILENO, buffer, read_status);
+		total += read_status;
+		letters -= BUFSIZE;
+	}
+	read_status = read(txt_file, buffer, letters);
+	write(STDOUT_FILENO, buffer, read_status);
+	total += read_status;
+	close(txt_file);
+	return (total);
+}
